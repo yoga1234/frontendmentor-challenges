ad29c4bca82a3b39b8a921b5bfe1697ef73ef7e4
Septiana Yoga <septianayoga30@gmail.com>
2022-03-16 14:23:28 +0700

    basic json for map data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from "react";
 
 import "./App.css";
 
-const App = () => "Hello World";
+function App() {
+  return (
+    <div className="mainapp-container">
+      <h1>List of Challenges</h1>
+      <div className="mainapp-list">
+        <p>Challenge will goes here</p>
+        <p>Challenge will goes here</p>
+        <p>Challenge will goes here</p>
+      </div>
+    </div>
+  );
+}
 
 export default App;
