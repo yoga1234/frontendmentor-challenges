f9971f37a2939a5c91f108402cff3a81ca56f89d
Septiana Yoga <septianayoga30@gmail.com>
2022-03-18 20:21:38 +0700

    initial code for advice generator

--- src/pages/AdviceGenerator/AdviceGenerator.js
+++ src/pages/AdviceGenerator/AdviceGenerator.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import "./AdviceGenerator.css";
+import {
+  iconDice,
+  patternDividerDesktop,
+  patternDividerMobile,
+} from "./images";
+
 function AdviceGenerator() {
-  return <div>AdviceGenerator</div>;
+  return (
+    <div className="advice-container">
+      <h3>Advice #117</h3>
+      <p>
+        "It is easy to sit up and take notice, what's difficult is getting up
+        and taking action"
+      </p>
+      <picture>
+        <source media="(min-width: 768px)" srcSet={patternDividerDesktop} />
+        <source media="(max-width: 767px)" srcSet={patternDividerMobile} />
+        <img src={patternDividerDesktop} alt="Pattern Divider" />
+      </picture>
+      <img src={iconDice} alt="Dice" />
+    </div>
+  );
 }
 
 export default AdviceGenerator;
---
+++ src/pages/AdviceGenerator/images/index.js
@@ -0,0 +1,5 @@
+import iconDice from "./icon-dice.svg";
+import patternDividerDesktop from "./pattern-divider-desktop.svg";
+import patternDividerMobile from "./pattern-divider-mobile.svg";
+
+export { iconDice, patternDividerDesktop, patternDividerMobile };
