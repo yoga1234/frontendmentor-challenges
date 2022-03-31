d533ea08dc3350ff6a028d305208fd2e099eeb53
Septiana Yoga <septianayoga30@gmail.com>
2022-03-31 15:04:42 +0700

    initial code sunnylandingpage header

--- src/pages/SunnyLandingpage/components/HeroImage/HeroImage.js
+++ src/pages/SunnyLandingpage/components/HeroImage/HeroImage.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "font-awesome/css/font-awesome.min.css";
 
 import "./HeroImage.css";
+import iconArrowDown from "../../images/icon-arrow-down.svg";
+import imageHeaderDesktop from "../../images/desktop/image-header.jpg";
 
 function HeroImage() {
   return (
     <div className="hero-container">
       <div className="hero-text">WE ARE CREATIVES</div>
-      <div className="arrow"></div>
-      <div className="orange-image"></div>
+      <img src={iconArrowDown} alt="arrow down" />
+      <img src={imageHeaderDesktop} alt="header desktop" />
     </div>
   );
 }
