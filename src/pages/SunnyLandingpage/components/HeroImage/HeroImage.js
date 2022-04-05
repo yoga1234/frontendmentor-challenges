744e899a294094665b8323d46356203d4bdc6a95
Septiana Yoga <septianayoga30@gmail.com>
2022-04-05 13:33:17 +0700

    structuring image header

--- src/pages/SunnyLandingpage/components/HeroImage/HeroImage.js
+++ src/pages/SunnyLandingpage/components/HeroImage/HeroImage.js
@@ -2,15 +2,23 @@ import React from "react";
 import "font-awesome/css/font-awesome.min.css";
 
 import "./HeroImage.css";
-import iconArrowDown from "../../images/icon-arrow-down.svg";
-import imageHeaderDesktop from "../../images/desktop/image-header.jpg";
+import {
+  desktopImageHeader,
+  mobileImageHeader,
+  iconArrowDown,
+} from "../../images";
 
 function HeroImage() {
   return (
     <div className="hero-container">
       <div className="hero-text">WE ARE CREATIVES</div>
       <img src={iconArrowDown} alt="arrow down" />
-      <img src={imageHeaderDesktop} alt="header desktop" />
+      {/* <img src={imageHeaderDesktop} alt="header desktop" /> */}
+      <picture>
+        <source media="(min-width: 426px)" srcset={desktopImageHeader} />
+        <source media="(min-width: 425px)" srcset={mobileImageHeader} />
+        <img src={desktopImageHeader} alt="Lemon blue background" />
+      </picture>
     </div>
   );
 }
---
+++ src/pages/SunnyLandingpage/images/index.js
@@ -0,0 +1,5 @@
+import desktopImageHeader from "./desktop/image-header.jpg";
+import mobileImageHeader from "./mobile/image-header.jpg";
+import iconArrowDown from "./icon-arrow-down.svg";
+
+export { desktopImageHeader, mobileImageHeader, iconArrowDown };
