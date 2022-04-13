f1c8fed826ad26d891bffc73080d5195dfb6141d
Septiana Yoga <septianayoga30@gmail.com>
2022-04-13 13:52:40 +0700

    layouting client testimonials

--- src/pages/SunnyLandingpage/components/ClientTestimonials/ClientTestimonials.js
+++ src/pages/SunnyLandingpage/components/ClientTestimonials/ClientTestimonials.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./ClientTestimonials.css";
+
 function ClientTestimonials() {
   return (
     <div className="client-testimonials">
@@ -13,6 +15,24 @@ function ClientTestimonials() {
           </p>
           <p className="client-name">Emily R.</p>
           <p className="client-job">Marketing Director</p>
+        </div>
+        <div className="testimonials-item">
+          <div className="img-goes-here"></div>
+          <p className="testimonial-description">
+            Sunnyside's enthusiasm coupled with their keen interest in our
+            brand's success made it a satisfying and enjoyable experience.
+          </p>
+          <p className="client-name">Thomas S.</p>
+          <p className="client-job">Chief Operating Officer</p>
+        </div>
+        <div className="testimonials-item">
+          <div className="img-goes-here"></div>
+          <p className="testimonial-description">
+            Incredible end result! Our sales increased over 400% when we worked
+            with Sunnyside. Highly recommended!
+          </p>
+          <p className="client-name">Jennie F.</p>
+          <p className="client-job">Business Owner</p>
         </div>
       </div>
     </div>
