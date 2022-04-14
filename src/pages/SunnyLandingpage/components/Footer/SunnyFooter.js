0fd815765bc1ac31e20114bb5d4a8e1bb96c5956
Septiana Yoga <septianayoga30@gmail.com>
2022-04-14 10:32:46 +0700

    finishing sunnyside footer without responsiveness

--- src/pages/SunnyLandingpage/components/Footer/SunnyFooter.js
+++ src/pages/SunnyLandingpage/components/Footer/SunnyFooter.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import "./SunnyFooter.css";
+
+import {
+  iconFacebook,
+  iconInstagram,
+  iconPinterest,
+  iconTwitter,
+} from "../../images";
+
 function SunnyFooter() {
-  return <div>SunnyFooter</div>;
+  return (
+    <div className="sunnyfooter-container">
+      <h3>sunnyside</h3>
+      <div className="footer-menu">
+        <ul>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+        </ul>
+      </div>
+      <div className="socmed-icons">
+        <img src={iconFacebook} alt="facebook" />
+        <img src={iconInstagram} alt="instagram" />
+        <img src={iconTwitter} alt="twitter" />
+        <img src={iconPinterest} alt="pinterest" />
+      </div>
+    </div>
+  );
 }
 
 export default SunnyFooter;
