d6f73c3270e02ea61ef2cb992f6cf3401b5b46ca
Septiana Yoga <septianayoga30@gmail.com>
2022-04-23 21:41:00 +0700

    fixing hamburger menu behaviour

--- src/pages/SunnyLandingpage/components/Navbar/Navbar.js
+++ src/pages/SunnyLandingpage/components/Navbar/Navbar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./Navbar.css";
 import { iconHamburger } from "../../images";
 
 function Navbar() {
+  useEffect(() => {
+    const hamburger = document.querySelector(".hamburger-icon");
+    const menuList = document.getElementById("menu-list");
+
+    hamburger.addEventListener("click", () => {
+      if (menuList.style.display === "block") {
+        menuList.style.display = "none";
+      } else {
+        menuList.style.display = "block";
+      }
+    });
+  }, []);
   return (
     <div className="navbar-container">
       <h2>sunnyside</h2>
@@ -12,8 +24,9 @@ function Navbar() {
           className="hamburger-icon"
           src={iconHamburger}
           alt="hamburger icon"
+          // onClick={menuClick}
         />
-        <ul>
+        <ul id="menu-list">
           <li>About</li>
           <li>Services</li>
           <li>Projects</li>
